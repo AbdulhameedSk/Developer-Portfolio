a643125b08a0337aaf90699ba8b326591d6def39
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-03-01 16:08:00 +0530

    Temporary stop

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Certifications</li>
+    </div>
+  )
+}
+
+export default Header
